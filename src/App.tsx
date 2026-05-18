5f9a7bb968b7ffffea93cf37c3aa21d091ecbec7
frank <fugangkuang4@gmail.com>
2026-05-18 18:00:30 +0800

    fix: rename index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage } from "./pages/Home/HomePage";
-import { SettingsPage } from "./pages/Settings/SettingsPage";
+import { HomePage } from "./pages/Home";
+import { SettingsPage } from "./pages/Settings";
 import { seedSettings, getSettings } from "./lib/api";
 import { useSettingsStore } from "./stores/settingsStore";
 import "./lib/i18n"; // i18next 初始化
